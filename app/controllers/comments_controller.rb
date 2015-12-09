f66d67d1bec38798f0e3c998d065fc442838b1da
paulsiskind <paulsiskind@gmail.com>
2015-12-09 10:56:27 -0700

    Something

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,6 +4,17 @@ class CommentsController < ApplicationController
     @comment = @post.comments.create(comment_params)
     redirect_to post_path(@post)
   end
+
+  def show
+    @comment = Comment.find(params[:id])
+  end
+
+  def destroy
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+    @comment.destroy
+    redirect_to post_path(@post)
+  end
  
   private
     def comment_params
