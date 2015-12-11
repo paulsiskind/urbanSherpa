fcb670d175c10d7b818078f4243a18b46cbbf4bc
paulsiskind <paulsiskind@gmail.com>
2015-12-10 18:52:42 -0700

    finished siginin

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   
+  get 'sessions/new'
+
   get 'signup'  => 'users#new'
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
 
   root 'welcome#index'
 
